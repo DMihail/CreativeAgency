ab597fd01d92b26286abab40259c5e312bd20cfb
devDMV <devDMV@devsteam.mobi>
2021-02-18 23:50:25 +0200

    add Welcome Message

--- src/component/Onboarding.js
+++ src/component/Onboarding.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import {Image, Dimensions} from 'react-native';
+import {Image, Dimensions, View} from 'react-native';
 import img from '../../assets/image/Subtract.png';
 import RadialGradient from 'react-native-radial-gradient';
+import WelcomeMessage from './OnboardingItems/WelcomeMessage';
 
 export const Onboarding = () => {
   const windowWidth = Dimensions.get('window').width;
   const windowHeight = Dimensions.get('window').height;
   return (
     <RadialGradient
-      style={{flex: 1, justifyContent: 'space-around', alignItems: 'center'}}
+      style={{flex: 1}}
       colors={['#23494E', '#042531']}
       stops={[0.0, 1.0]}
       center={[windowWidth / 2, windowHeight / 2]}
       radius={200}>
-      <Image source={img} />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <WelcomeMessage />
+        <Image source={img} />
+      </View>
     </RadialGradient>
   );
 };
